2e91b61c951bd83c56a4c407cf1b193ab7e83d36
NightFuryL <levodesskiy@gmail.com>
2026-05-02 11:10:27 +0300

    Classwork 02.05.2026 (First client network)

--- WinFormsTask/Form1.Designer.cs
+++ WinFormsTask/Form1.Designer.cs
@@ -28,12 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            btnSendRequest = new Button();
+            txtResult = new TextBox();
+            SuspendLayout();
+            // 
+            // btnSendRequest
+            // 
+            btnSendRequest.Location = new Point(12, 12);
+            btnSendRequest.Name = "btnSendRequest";
+            btnSendRequest.Size = new Size(776, 29);
+            btnSendRequest.TabIndex = 0;
+            btnSendRequest.Text = "Send Request";
+            btnSendRequest.UseVisualStyleBackColor = true;
+            btnSendRequest.Click += btnSendRequest_Click;
+            // 
+            // txtResult
+            // 
+            txtResult.Location = new Point(12, 47);
+            txtResult.Multiline = true;
+            txtResult.Name = "txtResult";
+            txtResult.ReadOnly = true;
+            txtResult.ScrollBars = ScrollBars.Vertical;
+            txtResult.Size = new Size(776, 391);
+            txtResult.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(txtResult);
+            Controls.Add(btnSendRequest);
+            Name = "Form1";
             Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button btnSendRequest;
+        private TextBox txtResult;
     }
 }
--- WinFormsTask/Form1.cs
+++ WinFormsTask/Form1.cs
@@ -1,10 +1,42 @@
-namespace WinFormsTask
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+namespace WinFormsTask;
+public partial class Form1 : Form
 {
-    public partial class Form1 : Form
+    public Form1()
     {
-        public Form1()
+        InitializeComponent();
+    }
+
+    private void btnSendRequest_Click(object sender, EventArgs e)
+    {
+        try
         {
-            InitializeComponent();
+            int serverPort = 8080;
+            IPAddress serverAddress = IPAddress.Loopback; //127.0.0.1
+            IPEndPoint serverEP = new IPEndPoint(serverAddress, serverPort);
+            using Socket clientSocket = new Socket(
+                AddressFamily.InterNetwork,//IPv4
+                SocketType.Stream,
+                ProtocolType.Tcp
+                );
+            clientSocket.Connect(serverEP);
+            string request = "GET / HTTP/1.1\r\nHost: localHost\r\nConenction: close\r\n\r\n";
+            clientSocket.Send(Encoding.UTF8.GetBytes(request));
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            txtResult.Clear();
+            do
+            {
+                bytesRead = clientSocket.Receive(buffer);
+                string receiveString = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                txtResult.AppendText(receiveString);
+            } while (bytesRead > 0);
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show($"ERROR: {ex.Message}");
         }
     }
 }
